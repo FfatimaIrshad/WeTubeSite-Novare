4b9826da241c85fa52100299ed9378eeecfc30c8
ffatimairshad <ffatimairshad@outlook.com>
2022-09-28 00:38:35 +0200

    filter button

---
+++ src/Components/FilterVideoButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./FilterVideoButton.css";
+
+function FilterButton() {
+  return (
+    <div className="FilterBar">
+      <ul>
+        <li className="main">All</li>
+        <li>Cricket</li>
+        <li>Comedy</li>
+        <li>Naruto</li>
+        <li>JavaScript</li>
+        <li>Thrillers</li>
+        <li>Trailer</li>
+        <li>Netflix</li>
+        <li>Game of Thrones</li>
+        <li>Football</li>
+        <li>Hotstar</li>
+        <li>Recently Uploaded</li>
+        <li>Live</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FilterButton;
