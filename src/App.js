ed2fb5474c378c7f087071494d5f0d312f15781c
ffatimairshad <ffatimairshad@outlook.com>
2022-09-28 20:28:47 +0200

    write comments

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "./App.css";
 import React from "react";
-import Header from "./Components/Header.js";
 import { BrowserRouter as Router } from "react-router-dom";
-import Sidebar from "./Components/SideBar";
-import FilterButton from "./Components/FilterVideoButton";
+import Header from "./Components/Header.js";
+{
+  /* Header */
+}
 
-import VideoList from "./Components/VideosList"; 
-// .....This is importing json file from VideosList folder >> VideosListData.json
+import Sidebar from "./Components/SideBar";
+{
+  /* Side bar */
+}
+import FilterButton from "./Components/FilterVideoButton";
+{
+  /* Filter Bar below header */
+}
+import VideoList from "./Components/VideosList";
+{
+  /* Json file named as video list because all video data was in here :) */
+}
 
 function App() {
   return (
     <Router>
-      <Header />                         {/* Header section aka navbar */}
-      <FilterButton />                   {/* Video Filter Buttons */}
-      <Sidebar />                        {/*  Side static navbar without links but styled */}
-      <VideoList/>
+      <Header /> {/* Header section aka navbar */}
+      <FilterButton /> {/* Video Filter Buttons */}
+      <Sidebar />
+      <VideoList />
+      {/*  Side static navbar without links but styled */}
     </Router>
   );
 }
